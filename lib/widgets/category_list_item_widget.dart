a7f2720087d65e4635529ac064506375a7484764
Ronak99 <punase.ronak99@gmail.com>
2019-09-13 21:05:40 +0530

    Added the showcase view dependency

---
+++ lib/widgets/category_list_item_widget.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery/const/themeColor.dart';
+
+
+class CategoryListItem extends StatelessWidget {
+  const CategoryListItem({
+    Key key,
+    @required this.categoryIcon,
+    @required this.categoryName,
+    @required this.availability,
+    @required this.selected,
+  }) : super(key: key);
+
+  final IconData categoryIcon;
+  final String categoryName;
+  final int availability;
+  final bool selected;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(right: 20),
+      padding: EdgeInsets.fromLTRB(10, 10, 10, 20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(50),
+        color: selected ? Themes.color : Colors.white,
+        border: Border.all(
+            color: selected ? Colors.transparent : Colors.grey[200],
+            width: 1.5),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey[100],
+            blurRadius: 15,
+            offset: Offset(15, 0),
+            spreadRadius: 5,
+          )
+        ],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(50),
+                border: Border.all(
+                    color: selected ? Colors.transparent : Colors.grey[200],
+                    width: 1.5)),
+            child: Icon(
+              categoryIcon,
+              color: Colors.black,
+              size: 30,
+            ),
+          ),
+          SizedBox(height: 10),
+          Text(
+            categoryName,
+            style: TextStyle(
+                fontWeight: FontWeight.w700, color: Colors.black, fontSize: 15),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(0, 6, 0, 10),
+            width: 1.5,
+            height: 15,
+            color: Colors.black26,
+          ),
+          Text(
+            availability.toString(),
+            style: TextStyle(
+              fontWeight: FontWeight.w600,
+              color: Colors.black,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/items_widget.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+class Items extends StatelessWidget {
+  Items({
+    @required this.leftAligned,
+    @required this.imgUrl,
+    @required this.itemName,
+    @required this.itemPrice,
+    @required this.hotel,
+  });
+
+  final bool leftAligned;
+  final String imgUrl;
+  final String itemName;
+  final double itemPrice;
+  final String hotel;
+
+  @override
+  Widget build(BuildContext context) {
+    double containerPadding = 45;
+    double containerBorderRadius = 10;
+
+    return Column(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.only(
+            left: leftAligned ? 0 : containerPadding,
+            right: leftAligned ? containerPadding : 0,
+          ),
+          child: Column(
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                height: 200,
+                decoration:
+                    BoxDecoration(borderRadius: BorderRadius.circular(10)),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.horizontal(
+                    left: leftAligned
+                        ? Radius.circular(0)
+                        : Radius.circular(containerBorderRadius),
+                    right: leftAligned
+                        ? Radius.circular(containerBorderRadius)
+                        : Radius.circular(0),
+                  ),
+                  child: Image.network(
+                    imgUrl,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                  padding: EdgeInsets.only(
+                    left: leftAligned ? 20 : 0,
+                    right: leftAligned ? 0 : 20,
+                  ),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: Text(itemName,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w700,
+                                    fontSize: 18,
+                                  )),
+                            ),
+                            Text("\$$itemPrice",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 18,
+                                )),
+                          ],
+                        ),
+                        SizedBox(height: 10),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: RichText(
+                            text: TextSpan(
+                                style: TextStyle(
+                                    color: Colors.black45, fontSize: 15),
+                                children: [
+                                  TextSpan(text: "by "),
+                                  TextSpan(
+                                      text: hotel,
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w700))
+                                ]),
+                          ),
+                        ),
+                        SizedBox(height: containerPadding),
+                      ])),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
